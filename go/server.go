b037fd945e75301cecce505275da31324a22c9c2
Tuni-Soft <amxmodx.unloco@gmail.com>
2025-10-20 20:12:04 +0100

    Add server file

---
+++ go/server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func main() {
+	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+	app.Static("/", "./frontend/build")
+	app.Get("/", func(c *fiber.Ctx) error {
+		// Get raw body from POST request:
+		return c.SendString("OK2") // []byte("user=john")
+	})
+	app.Post("/", func(c *fiber.Ctx) error {
+		// Get raw body from POST request:
+		return c.Send(c.BodyRaw()) // []byte("user=john")
+	})
+	app.Listen(":9999")
+}
