4a1cce9c290c59cd95c0b715f40a53b1c4d74290
Tuni-Soft <amxmodx.unloco@gmail.com>
2025-10-31 16:16:34 +0100

    Reformat

--- src/routes/users/+page.svelte
+++ src/routes/users/+page.svelte
@@ -8,7 +8,7 @@
 <h1>SvelteKit Connector for Go</h1>
 <p>Connect your SvelteKit frontend to Go backend easily using this library.</p>
 
-<button onclick={async () => (infos = await QueryUserInfo())}> Fetch user info</button>
+<button onclick={async () => (infos = await QueryUserInfo())}> Fetch user info </button>
 
 <div>
 	{JSON.stringify(infos)}
